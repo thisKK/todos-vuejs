522ca75880e0d8e3d789fef32399e7606bb23374
thisKK <42506615+thisKK@users.noreply.github.com>
2021-03-14 01:28:23 +0700

    add checkall item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,20 +1,49 @@
 <template>
   <div>
     <input
-      v-model="newTodo"
-      @keyup.enter="addTodo"
       type="text"
       class="todo-input"
       placeholder="What needs to be done"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
     />
-    <div v-for="todo in todos" :key="todo.id" class="todo-item">
-      <div>
-        {{ todo.title }}
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <input type="checkbox" v-model="todo.completed" />
+        <div
+          v-if="!todo.editing"
+          @dblclick="editTodo(todo)"
+          class="todo-item-label"
+          :class="{ completed: todo.completed }"
+        >
+          {{ todo.title }}
+        </div>
+        <input
+          v-else
+          class="todo-item-edit"
+          type="text"
+          v-model="todo.title"
+          @blur="doneEdit(todo)"
+          @keyup.enter="doneEdit(todo)"
+          @keyup.esc="cancelEdit(todo)"
+          v-focus
+        />
       </div>
-      <div class="remove-item">
+      <div class="remove-item" @click="removeTodo(index)">
         &times;
       </div>
     </div>
+
+    <div class="extra-container">
+      <div>
+        <label>
+          <input type="checkbox" :checked="!anyRemaining" 
+          @change="checkAllTodos"/> Check All
+        </label>
+      </div>
+      <div>{{ remaining }} items left</div>
+    </div>
+    
   </div>
 </template>
 
@@ -25,37 +54,73 @@ export default {
     return {
       newTodo: "",
       idForTodo: 3,
+      beforeEditCache: "",
+      filter: "all",
       todos: [
         {
           id: 1,
-          title: "Finish",
+          title: "Finish Vue Screencast",
           completed: false,
+          editing: false,
         },
         {
           id: 2,
-          title: "Take over",
+          title: "Take over world",
           completed: false,
+          editing: false,
         },
       ],
     };
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter((todo) => !todo.completed).length;
+    },
+    anyRemaining() {
+      return this.remaining != 0;
+    },
   },
   methods: {
     addTodo() {
       if (this.newTodo.trim().length == 0) {
         return;
       }
-
       this.todos.push({
         id: this.idForTodo,
         title: this.newTodo,
         completed: false,
+        editing: false,
       });
       this.newTodo = "";
       this.idForTodo++;
     },
+    editTodo(todo) {
+      this.beforeEditCache = todo.title;
+      todo.editing = true;
+    },
+    doneEdit(todo) {
+      if (todo.title.trim() == "") {
+        todo.title = this.beforeEditCache;
+      }
+      todo.editing = false;
+    },
+    cancelEdit(todo) {
+      todo.title = this.beforeEditCache;
+      todo.editing = false;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    checkAllTodos() {
+      this.todos.forEach((todo) => todo.completed = event.target.checked)
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter((todo) => !todo.completed);
+    },
   },
 };
 </script>
+
 <style lang="scss">
 @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
 
@@ -68,7 +133,6 @@ export default {
     outline: 0;
   }
 }
-
 .todo-item {
   margin-bottom: 12px;
   display: flex;
@@ -76,12 +140,69 @@ export default {
   justify-content: space-between;
   animation-duration: 0.3s;
 }
-
 .remove-item {
   cursor: pointer;
   margin-left: 14px;
   &:hover {
     color: black;
   }
+}
+.todo-item-left {
+  // later
+  display: flex;
+  align-items: center;
+}
+.todo-item-label {
+  padding: 10px;
+  border: 1px solid white;
+  margin-left: 12px;
+}
+.todo-item-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc; //override defaults
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  &:focus {
+    outline: none;
+  }
+}
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  border-top: 1px solid lightgrey;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
+button {
+  font-size: 14px;
+  background-color: white;
+  appearance: none;
+  &:hover {
+    background: lightgreen;
+  }
+  &:focus {
+    outline: none;
+  }
+}
+.active {
+  background: lightgreen;
+}
+// CSS Transitions
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
