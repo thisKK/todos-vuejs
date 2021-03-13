d86031782ad8205525376c918e885066df8224a2
thisKK <42506615+thisKK@users.noreply.github.com>
2021-03-14 03:44:04 +0700

    eventBus not working

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+window.eventBus = new Vue()
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
