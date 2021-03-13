6b33cfdd59f79b776c73d5929c8f6e26ff233ce6
thisKK <sonicrat12@gmail.com>
2021-03-13 20:22:01 +0700

    add todo item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,43 @@
 <template>
   <div>
-    from todo list 
+    <input v-model="newTodo" type="text" class="todo-input" placeholder="What needs to be done">
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{todo.title}}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'todo-list',
+  data(){
+    return{
+      newTodo: '',
+      todos:[
+        {
+          'id': 1,
+          'title': 'Finish',
+          'completed':false,
+        },
+         {
+          'id': 2,
+          'title': 'Take over',
+          'completed':false,
+        },
+      ]
+    }
+  }
 }
 </script>
+<style lang="scss">
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus{
+      outline:0px;
+    }
+  }
+</style>
