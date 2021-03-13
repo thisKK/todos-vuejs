23fd59bf6f2e3f273d9b4d470ec4460b6dee5617
thisKK <sonicrat12@gmail.com>
2021-03-13 20:09:04 +0700

    create todolist componance

---
+++ src/components/TodoList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    from todo list 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <img alt="Vue logo" class="logo" src="../assets/logo.png">
+    <TodoList></TodoList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TodoList from '../components/TodoList.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
+
+<style lang="scss">
+*{
+  box-sizing: border-box;
+}
+.container{
+  max-width: 600px;
+  margin: 0 auto;
+}
+.logo{
+  display: block;
+  margin: 20px auto;
+  height: 75px;
+}
+</style>
